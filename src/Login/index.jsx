1ff0d5709f358bf1ae820cc35cddaede41954f1f
Patricia <patricia.ribeiro714@gmail.com>
2023-03-31 10:09:31 -0300

    update-

--- src/Login/index.jsx
+++ src/Login/index.jsx
@@ -3,14 +3,16 @@ import './Login.css'
 export function Login() {
     return (
         <div className='container-login'>
-            <h1 className='h1-login'>Hello Login</h1>
-            <input type="text" />
-            <input type="text" />
-           <div className='gruop-btn'>
-            <button>Entrar</button>
-            <button>Cadastrar</button>
-           </div>
+            <div className="login-form">
+                <h1 className='h1-login'>Hello Login</h1>
+                <input type="text" />
+                <input type="text" />
+                <div className='group-btn'>
+                    <button>Entrar</button>
+                    <button>Cadastrar</button>
+                </div>
+            </div>
         </div>
     )
-    
+
 }
